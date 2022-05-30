8d354994eb1f08c825a9c462ba8fe9f1bbd73f47
Nguyen <Nguyen@LAPTOP-0GJ9DMA0>
2022-05-30 11:12:50 +0700

    Add project files.

---
+++ Controllers/CoursesController.cs
@@ -0,0 +1,29 @@
+﻿using LabBigSchool.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LabBigSchool.Migrations;
+using LabBigSchool.ViewModels;
+
+namespace LabBigSchool.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        // GET: Empty
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel()
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
